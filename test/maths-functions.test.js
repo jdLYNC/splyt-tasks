9ace51605e0c3053848dc4d5814628cd3c403d1e
Josh <josh.lynch@voxlydigital.com>
2019-01-18 13:46:00 +0000

    created maths functions tests from supplied examples [failing]

---
+++ test/maths-functions.test.js
@@ -0,0 +1,21 @@
+const { expect } = require('chai');
+
+describe('Maths Functions', () => {
+
+  it('should handle 7 * 5', () => {
+    expect(seven(times(five()))).to.equal(35);
+  });
+
+  it('should handle 4 + 9', () => {
+    expect(four(plus(nine()))).to.equal(13);
+  });
+
+  it('should handle 8 - 3', () => {
+    expect(eight(minus(three()))).to.equal(5);
+  });
+
+  it('should handle 6 / 2', () => {
+    expect(six(dividedBy(two()))).to.equal(3);
+  });
+
+});
